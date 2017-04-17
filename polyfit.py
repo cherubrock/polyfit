939382404a5f1a0becf305162f2a1469a6be4c8e
Zimon <szymon.basan@gmail.com>
2017-04-17 02:01:23 +0200

    blob

--- polyfit.py
+++ polyfit.py
@@ -1,14 +1,12 @@
 from sympy import *
-from sympy.plotting import plot
 
-def makepoly_even(proto,x_arr,dx_arr):
+def make_poly_even(proto,x_arr,dx_arr):
   # generates a polynomial fitting given curve
   # proto = target expression/function eg. sin(x), the curve wil be fitted against function of x argument (x symbol is mandatory)
   # x_arr = list of x arguments values for which there will be exact match between proto and resulting polynomial
   # dx_arr = list of x arguments values for which there will be exact match between proto first derivative and resulting polynomial first derivative
   proto_diff = proto.diff()
-  a = symbols('a0,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15')
-  a = a[:len(x_arr+dx_arr)]
+  a = symbols(make_coeff_symbols(len(x_arr+dx_arr)))
   # insert zeros for odd coefficients
   coeffs = [val for pair in zip([0]*len(a),list(reversed(a))) for val in pair]
   poly = Poly(coeffs,gens=x)
@@ -26,12 +24,19 @@ def makepoly_even(proto,x_arr,dx_arr):
   #for eqn in eqns:
   #  print eqn
   solution = poly.subs(solve(eqns,a))
-  #plot(solution,(x,0,0.5));
   #print solution
   return solution
 
+def make_coeff_symbols(n,prefix='a'):
+  symlist = []
+  for i in range(n):
+    symlist.append(prefix+str(i))
+  return ','.join(symlist)
+
+
 x = Symbol('x')
-makepoly_even(cos(pi*x),["0","1/4","1/2"],["1/4","1/2"])
-makepoly_even(cos(pi*x/2),["0","1"],["1"])
+print make_poly_even(cos(pi*x),["0","1/4","1/2"],["1/4","1/2"])
+print make_poly_even(cos(pi*x/2),["0","1"],["1"])
+
 
   
